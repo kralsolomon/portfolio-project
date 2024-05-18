2621084595cef47bbf93dae5d9f4fd9904f99fa8
kralsolomon <solomonkein228@gmail.com>
2024-05-19 00:42:16 +0500

    deploy to gh

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: "/https://github.com/kralsolomon/portfolio-project",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
