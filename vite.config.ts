235d63d8e5c0030574b2399bb6d1670cc728d11f
kralsolomon <solomonkein228@gmail.com>
2024-05-19 00:59:32 +0500

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/https://github.com/kralsolomon/portfolio-project",
+  base: "/portfolio-project/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
